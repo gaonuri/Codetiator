9ee84fa211ddbffe2cca9729f932da26b27b1236
USER <USER@USER-PC>
2019-07-16 17:42:58 +0900

    공지사항 생성

--- creator/src/main/java/kr/co/creator/support/SupportController.java
+++ creator/src/main/java/kr/co/creator/support/SupportController.java
@@ -1,11 +1,19 @@
 package kr.co.creator.support;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.co.creator.vo.NoticeVO;
+
 
 @Controller
 public class SupportController {
@@ -15,35 +23,95 @@ public class SupportController {
 	@Autowired
 	SupportService service;
 	
-	@RequestMapping(value = "/faq", method = RequestMethod.GET)
-	public String faq() {
-		logger.info("faq");
-		
-		service.faq();
-		
-		return "support/faq";
-	}
-	
+//	@RequestMapping(value = "/faq", method = RequestMethod.GET)
+//	public String faq() {
+//		logger.info("faq");
+//		
+//		
+//		return "support/faq";
+//	}
+//	
+//	@RequestMapping(value = "/policy", method = RequestMethod.GET)
+//	public String policy() {
+//		logger.info("policy");
+//		
+//		return "support/policy";
+//	}
+//	
+//	@RequestMapping(value = "/inquiry", method = RequestMethod.GET)
+//	public String inquiry() {
+//		logger.info("inquiry");
+//				
+//		return "support/inquiry";
+//	}
+//	
 	@RequestMapping(value = "/support", method = RequestMethod.GET)
-	public String support() {
+	public String support(Model model) {
 		logger.info("support");
-		
-		service.support();
-		
+		List<NoticeVO> support = null;
+		support = service.supportList();
+		model.addAttribute("supportlist", support);
 		return "support/support";
 	}
 	
-	@RequestMapping(value = "/policy", method = RequestMethod.GET)
-	public String policy() {
-		logger.info("policy");
-		
-		return "support/policy";
-	}
+	@RequestMapping(value="/support/formi"
+			,method=RequestMethod.GET)
+	public String formInsert() {
+	logger.info("formInsert");
+	return "support/form_insert";
+	}//supportInsert
 	
-	@RequestMapping(value = "/inquiry", method = RequestMethod.GET)
-	public String inquiry() {
-		logger.info("inquiry");
-				
-		return "support/inquiry";
-	}
+	@RequestMapping(value="/support/insert"
+			,method=RequestMethod.POST)
+	public void supportInsert(NoticeVO vo, PrintWriter out) {
+	System.out.println("supportInsert");
+	int count = 0;
+	count = service.supportInsert(vo);
+	logger.info("controller - count : "+count);
+	out.print(count);
+	out.flush();
+	out.close();
+	}//boardInsert
+	
+//	@RequestMapping(value="/support/formu"
+//			,method=RequestMethod.GET)
+//	public String formUpdate(NoticeVO vo, Model model) {
+//	logger.info("supportUpdate");
+//	vo = service.boardDetail(vo);
+//	//CK image start============================
+//	int imgYn = 0;
+//	imgYn = vo.getCnts().indexOf("src=\"");
+//	if(imgYn > 0) {//image in cnts
+//	try {
+//	FileUtilService.mvCKImgNewToTmp(
+//			vo.getCnts(), vo.getNo());
+//	} catch (IOException e) {
+//	e.printStackTrace();
+//	}//try
+//	}//if
+//	//CK image end============================
+//	vo.setCnts(vo.getCnts().replaceAll("/"+vo.getNo()+"/"
+//	, "/tmp/")
+//	);
+//	model.addAttribute("detailVO", vo);
+//	return "board1/form_update";
+//	}//formUpdate
+//
+//	
+//	
+//	@RequestMapping(value="/support/delete"
+//			,method=RequestMethod.GET)
+//	public void boardDelete(NoticeVO vo, PrintWriter out) {
+//	logger.info("supportDelete");
+//	//CK image delete start==================
+//	FileUtilService.delCKImg(vo.getNo());
+//	//CK image delete end==================
+//	int count = 0;
+//	count = service.supportDelete(vo);
+//	out.print(count);
+//	out.flush();
+//	out.close();
+//	}//boardDelete
+	
+	
 }//class
--- creator/src/main/java/kr/co/creator/support/SupportDAO.java
+++ creator/src/main/java/kr/co/creator/support/SupportDAO.java
@@ -1,8 +1,12 @@
 package kr.co.creator.support;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import kr.co.creator.vo.NoticeVO;
 
 @Repository
 public class SupportDAO {
@@ -10,7 +14,18 @@ public class SupportDAO {
 	@Autowired
 	SqlSession sqlSession;
 
-	public void support() {
-		//sqlSession.selectList("SupportMapper.");
+	public int supportInsert(NoticeVO vo) {
+		System.out.println("dao - supportInsert");
+		int count = 0;
+		count = sqlSession.insert("SupportMapper.supportInsert", vo);
+		System.out.println("dao - count : "+ count);
+		return count;
+	}//noticeInsert
+
+	public List<NoticeVO> supportList() {
+		List<NoticeVO> support = null;
+		support = sqlSession.selectList(
+							"SupportMapper.supportList");
+		return support;
 	}
 }//class
