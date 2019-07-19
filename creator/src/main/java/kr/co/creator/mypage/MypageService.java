3863b5a5bf99aec35f57ee89408222426719f40a
doyeobi <doyeobi@DESKTOP-ESGOAQH>
2019-07-19 13:44:22 +0900

    tt

--- creator/src/main/java/kr/co/creator/mypage/MypageService.java
+++ creator/src/main/java/kr/co/creator/mypage/MypageService.java
@@ -21,11 +21,7 @@ public class MypageService {
 	}//assetsList
 
 	public ProjectVO project_detail(ProjectVO proVO) {
-<<<<<<< HEAD
 		proVO = dao.project_detail(proVO);
-=======
-		proVO = MypageDAO.project_detail(proVO);
->>>>>>> branch 'master' of https://github.com/gaonuri/Codetiator.git
 		return proVO;
 	}//project_detail
 
