c5cfb37eaedfeb03ad0ea87bd9901018f17fce9b
USER <USER@USER-PC>
2019-07-24 19:00:10 +0900

    fff

--- creator/src/main/java/kr/co/creator/mypage/MypageController.java
+++ creator/src/main/java/kr/co/creator/mypage/MypageController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.co.creator.vo.InvestVO;
+import kr.co.creator.vo.MemberVO;
 import kr.co.creator.vo.MypageVO;
 import kr.co.creator.vo.NoticeVO;
 import kr.co.creator.vo.ProjectVO;
@@ -28,15 +29,22 @@ public class MypageController {
 	MypageService service;
 	
 	@RequestMapping(value = "/my_dashboard", method = RequestMethod.GET)
-	public String list(HttpSession session, Model model, MypageVO userVO) {
+	public String list(HttpSession session, Model model, MemberVO userVO, MypageVO myVO) {
 		logger.info("my_dashboard");
-		List<MypageVO> invest = null;   // 전체
-
-		UserVO uvo = (UserVO)session.getAttribute("userVO");
+		userVO = (MemberVO)session.getAttribute("userVO");
+		System.out.println("MemverVO");
 		session.getAttribute("busiUserVO");
+		System.out.println("Controller 111111111111111111111111 : " + userVO);
 		
-		userVO.setUser_num("user_num");
-		invest = service.invest_detail(uvo);
+		List<MypageVO> invest = null;
+		invest = service.invest_detail();
+		model.addAttribute("investList", invest);
+		
+//		userVO.setUser_num("user_num");
+//		
+//		userVO.setBusi_num("busi_num");
+//		service.invest_detail(userVO);
+//		
 		
 		return "mypage/my_dashboard";
 	}
