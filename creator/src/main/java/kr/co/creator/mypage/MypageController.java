9e8f923b837905a8ce6ec8d313a898317f4558ba
doyeobi <doyeobi@DESKTOP-ESGOAQH>
2019-08-13 07:59:55 +0900

    dddddddddddddddddddddddddddddddddddddddddddddo

--- creator/src/main/java/kr/co/creator/mypage/MypageController.java
+++ creator/src/main/java/kr/co/creator/mypage/MypageController.java
@@ -49,24 +49,6 @@ public class MypageController {
 
 	@Autowired
 	private EmailSender emailSender;
-	
-	@RequestMapping(value = "/my_dashboard", method = RequestMethod.GET)
-	public String list(HttpSession session, Model model, MemberVO userVO, MypageVO myVO) {
-		logger.info("my_dashboard");
-		userVO = (MemberVO)session.getAttribute("memberVO");
-		List<MypageVO> invest = null;
-		invest = service.invest_detail(userVO);
-		model.addAttribute("investList", invest);
-		return "mypage/my_dashboard";
-	}
-	
-	@RequestMapping(value = "/my_dashboard_new", method = RequestMethod.GET)
-	public String my_dashboard_new(HttpSession session, Model model, MemberVO userVO, MypageVO myVO) {
-		logger.info("my_dashboard_new");
-
-		return "mypage/my_dashboard_new";
-	}//my_dashboard_new
-	
 	@RequestMapping(value = "/my_invest_list", method = RequestMethod.GET)
 	public String invest(HttpSession session, Model model, MemberVO userVO, MypageVO myVO) {
 		logger.info("my_dashboard");
@@ -74,18 +56,12 @@ public class MypageController {
 		List<MypageVO> invest = null;
 		invest = service.invest_detail(userVO);
 		model.addAttribute("investList", invest);
+		model.addAttribute("investuser", userVO);
 		return "mypage/my_invest_list";
 	}
 	
-	@RequestMapping(value = "/my_invest_list_new", method = RequestMethod.GET)
-	public String my_invest_list_new(HttpSession session, Model model, MemberVO userVO, MypageVO myVO) {
-		logger.info("my_invest_list_new");
-
-		return "mypage/my_invest_list_new";
-	}//my_invest_list_new
-	
 	@RequestMapping(value = "/my_loan_list", method = RequestMethod.GET)
-	public String loan(HttpSession session, Model model, MemberVO userVO, ProjectVO proVO) {
+	public String loan(HttpSession session, Model model, MemberVO userVO, ProjectVO proVO, MypageVO myVO) {
 		logger.info("my_loan_list");
 		userVO = (MemberVO)session.getAttribute("memberVO");
 		List<ProjectVO> loan= null;
@@ -93,13 +69,6 @@ public class MypageController {
 		model.addAttribute("loanList",loan);
 		return "mypage/my_loan_list";
 	}
-	
-	@RequestMapping(value = "/my_loan_list_new", method = RequestMethod.GET)
-	public String my_loan_list_new(HttpSession session, Model model, MemberVO userVO, ProjectVO proVO) {
-		logger.info("my_loan_list_new");
-
-		return "mypage/my_loan_list_new";
-	}//my_loan_list_new
 	
 	@RequestMapping(value = "/my_depo_mgn", method = RequestMethod.GET)
 	public String my_modify(HttpSession session, Model model, MemberVO userVO, UserVO useVO, Busi_userVO busiVO, AccountVO accVO, InOutVO ioVO) {
